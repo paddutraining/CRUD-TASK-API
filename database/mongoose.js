e4a9e8c2b6812964f4b34504f1644c837f4e18ca
coalescesekhar <coalesce.sekhar@gmail.com>
2022-04-16 22:37:23 +0530

    reveiwed comment fixed. please review and approve

--- database/mongoose.js
+++ database/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose=require('mongoose');
 mongoose.Promise=global.Promise;
 mongoose.connect('mongodb://127.0.0.1:27017/mongodb').then(()=>console.log("db connected successfully")).catch((error)=>{
-    console.log(error)
+    console.log("connect failed due to :"+error)
 })
 module.exports=mongoose;
 
