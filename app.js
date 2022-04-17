2f20f874f93174118984ad286a91db48f0444317
coalescesekhar <coalesce.sekhar@gmail.com>
2022-04-17 12:47:14 +0530

    task update delete route path added

--- app.js
+++ app.js
@@ -50,6 +50,45 @@ app.post('/tasklists', (req,res)=>{
         console.log(console.log(error))})
 
 })
+// get all task from particullar tasklist
+app.get('/tasklists/:tasklistid/tasks', (req,res)=>{
+    task.find({_tasklistid:req.params.tasklistid}).then((list)=>{res.status(200).send(list)}).catch((error)=>{
+        res.status(500)
+        console.log(error)})
+    })
+// get all task from particullar tasklist
+// creat task in particular tasklist
+app.post('/tasklists/:tasklistid/tasks', (req,res)=>{
+    let taskobj={'title':req.body.title, 
+    '_tasklistid':req.params.tasklistid};
+    task(taskobj).save().then((list)=>{res.status(200).send(list)}).catch((error)=>{
+        res.status(500)
+        console.log(error)})
+    })
+// creat task in particular tasklist
+// get 1 task from particullar tasklist
+app.get('/tasklists/:tasklistid/tasks/:taskid', (req,res)=>{
+    task.findOne({_tasklistid:req.params.tasklistid
+    ,_id:req.params.taskid}).then((list)=>{res.status(200).send(list)}).catch((error)=>{
+        res.status(500)
+        console.log(error)})
+    })
+// get 1 task from particullar tasklist
+// patch task in particular tasklist
+app.patch('/tasklists/:tasklistid/tasks/:taskid', (req,res)=>{
+    task.findOneAndUpdate({_tasklistid:req.params.tasklistid,id:req.params.taskid},
+        {$set:req.body}).then((list)=>{res.status(200).send(list)}).catch((error)=>{
+        res.status(500)
+        console.log(error)})
+    })
+// creat task in particular tasklist
+// delete task in particular tasklist
+app.delete('/tasklists/:tasklistid/tasks/:taskid', (req,res)=>{
+    task.findOneAndDelete({_tasklistid:req.params.tasklistid,id:req.params.taskid}).then((list)=>{res.status(200).send(list)}).catch((error)=>{
+        res.status(500)
+        console.log(error)})
+    })
+// creat task in particular tasklist
 app.listen(3000, ()=>{
     console.log("server started success fully on port number 3000");
 })
